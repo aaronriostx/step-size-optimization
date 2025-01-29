713d32311f8cbce6d9c1fde73736f4f41aef4752
G. Aaron Rios <gaaronrios@gmail.com>
2025-01-29 13:19:37 -0600

    WIP: adding parser for CLI

--- sso_package/gill_step_size_optimizer.py
+++ sso_package/gill_step_size_optimizer.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
+import argparse
 import numpy as np
+import pathlib
+import sys
 
 import sso_package.initial_hs
 import sso_package.central_difference
@@ -71,4 +74,36 @@ def optimize_step_size(func, evaluation_point, error_bound, conditional_error_bo
         print(f'conditional error: {cond_error}')
         print(f'optimized step size: {h_optimal}')
     
-    return h_optimal
+    return 0
+
+def get_parser():
+    script_name = pathlib.Path(__file__)
+    prog = f"python {script_name.name} "
+    cli_description = "Optimize the forward finite difference (FFD) step size using Gill's method"
+    parser=argparse.ArgumentParser(description=cli_description, prog=prog)
+
+    parser.add_argument('--func', required=True,
+        help="A single-variable function used to estimate the FFD approximation")
+    parser.add_argument('--evaluation_point', type=float, required=True,
+        help="The evaluation point for the FFD approximation")
+    parser.add_argument('--error_bound', type=float, required=True,
+        help="The user-defined error bound, typically it is the machine precision")
+    parser.add_argument('--conditional_error_bounds', type=tuple, required=True,
+        help="The user-defined error bound threshold")
+    parser.add_argument('--max_iters', type=float, default=50, required=False,
+        help="Max iterations to optimize the step size. Default is 50.")
+    parser.add_argument('--print_info', type=bool, default=True, required=False,
+        help="Print optimzer information to the console")
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args, unknown = parser.parse_known_args()
+    sys.exit(optimize_step_size(func=args.func,
+                                evaluation_point=args.evaluation_point,
+                                error_bound=args.error_bound,
+                                conditional_error_bounds=args.conditional_error_bounds,
+                                max_iters=args.max_iters,
+                                print_info=args.print_info
+                                ))
