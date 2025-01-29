8b7de7c90bc7eac18ca34c974495a817f8067121
G. Aaron Rios <gaaronrios@gmail.com>
2025-01-29 14:43:07 -0600

    WIP: new file to define analytical function

--- sso_package/myfunc.py
+++
@@ -1,7 +0,0 @@
-def my_func(x):
-    try:
-        return np.exp(x)/np.sqrt(np.sin(x)**3 + np.cos(x)**3)
-    except RuntimeWarning:
-        return 0
-    except ZeroDivisionError:
-        return 0
