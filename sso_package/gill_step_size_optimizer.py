ba2496b28b655a9aa9b6cdc86ebfda44241cd8b8
G. Aaron Rios <gaaronrios@gmail.com>
2025-01-29 14:44:30 -0600

    WIP: it is working, but need to work on namespace and package organization

--- sso_package/gill_step_size_optimizer.py
+++ sso_package/gill_step_size_optimizer.py
@@ -5,20 +5,20 @@ import numpy as np
 import pathlib
 import sys
 
-import sso_package.initial_hs
-import sso_package.central_difference
-import sso_package.conditional_error
-import sso_package.optimal_step_size
-import sso_package.machine_precision
+import sso_package.initial_hs as initial_hs
+import sso_package.central_difference as central_difference
+import sso_package.conditional_error as conditional_error
+import sso_package.optimal_step_size as optimal_step_size
+import sso_package.machine_precision as machine_precision
+import sso_package.my_func as my_func
 
-def optimize_step_size(func, evaluation_point, lower_c_bound, upper_c_bound, error_bound=None, max_iters=50, print_info=False):
+def optimize_step_size(evaluation_point, lower_c_bound, upper_c_bound, error_bound=None, max_iters=50, print_info=False):
     """Runs the algorithm to determine optimal step size for first derivative of a function 
 
     User-defined function is used and evaluated at the evaluation point. The algorithm
     uses passed in conditional error bounds and error bound parameters to minimize the total error
     on the finite difference calculation.
     
-    :param function func: Analytical function that takes in single evaluation point
     :param float evaluation_point: Evaluation point passed into the function
     :param float lower_c_bound: Lower bound to threshold the conditional error
     :param float upper_c_bound: Upper bound to threshold the conditional error
@@ -28,21 +28,26 @@ def optimize_step_size(func, evaluation_point, lower_c_bound, upper_c_bound, err
 
     :returns: optimized step size for the finite difference calculation
     """
+    # Set the variable equal to the single-variable function
+    func = my_func.my_func
+    print(func(1))
+
     # Evaluation point
     x1 = evaluation_point
-     
-    # Compute initial step size
-    hs_initial = initial_hs(func, x1, error_bound)
-    
-    # Compute initial phi
-    phi = central_difference(func, x1, hs_initial)
-    
-    # Compute  initial conditional error
-    cond_error = conditional_error(error_bound, hs_initial, phi)
+    print(x1)
 
     # Get machine precision if no value is passed for error_bound
     if error_bound == None:
-        error_bound = sso_package.machine_precision()
+        error_bound = machine_precision.machine_precision()
+     
+    # Compute initial step size
+    hs_initial = initial_hs.initial_hs(func, x1, error_bound)
+    
+    # Compute initial phi
+    phi = central_difference.central_difference(func, x1, hs_initial)
+    
+    # Compute  initial conditional error
+    cond_error = conditional_error.conditional_error(error_bound, hs_initial, phi)
 
     # Initialize iterations to prevent infinite looping
     iter = 0
@@ -53,14 +58,14 @@ def optimize_step_size(func, evaluation_point, lower_c_bound, upper_c_bound, err
         # Update hs and phi if conditional error is out of bounds
         if cond_error > upper_c_bound:
             hs = hs_initial*10
-            phi = central_difference(func, x1, hs)
+            phi = central_difference.central_difference(func, x1, hs)
         
         elif cond_error < lower_c_bound:
             hs = hs_initial/10
-            phi = central_difference(func, x1, hs)
+            phi = central_difference.central_difference(func, x1, hs)
         
         # Compute conditional error with updated values
-        cond_error = conditional_error(error_bound, hs, phi)
+        cond_error = conditional_error.conditional_error(error_bound, hs, phi)
 
         # Check number of iterations
         iter += 1
@@ -68,7 +73,7 @@ def optimize_step_size(func, evaluation_point, lower_c_bound, upper_c_bound, err
             break
         
     # Return the optimal step size
-    h_optimal = optimal_step_size(error_bound, phi)
+    h_optimal = optimal_step_size.optimal_step_size(error_bound, phi)
     
     # Print information to console
     if print_info == True:
@@ -84,13 +89,11 @@ def get_parser():
     cli_description = "Optimize the forward finite difference (FFD) step size using Gill's method"
     parser=argparse.ArgumentParser(description=cli_description, prog=prog)
 
-    parser.add_argument('--func', type=function, required=True,
-        help="A single-variable function used to estimate the FFD approximation")
     parser.add_argument('--evaluation_point', type=float, required=True,
         help="The evaluation point for the FFD approximation")
-    parser.add_argument('--lower_c_bound', type=float, default=0.001
+    parser.add_argument('--lower_c_bound', type=float, default=0.001,
         help="The lower error bound threshold. Default: 0.001")
-    parser.add_argument('--upper_c_bound', type=float, default=0.1
+    parser.add_argument('--upper_c_bound', type=float, default=0.1,
         help="The upper error bound threshold. Default: 0.1")
     parser.add_argument('--error_bound', type=float, required=False,
         help="The user-defined error bound, typically it is the machine precision")
@@ -104,8 +107,7 @@ def get_parser():
 if __name__ == '__main__':
     parser = get_parser()
     args, unknown = parser.parse_known_args()
-    sys.exit(optimize_step_size(func=args.func,
-                                evaluation_point=args.evaluation_point,
+    sys.exit(optimize_step_size(evaluation_point=args.evaluation_point,
                                 error_bound=args.error_bound,
                                 lower_c_bound=args.lower_c_bound,
                                 upper_c_bound=args.upper_c_bound,
